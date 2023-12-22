d2bcd3d1b2ed4fe28a40775b6ad1fe0354803a37
emirhan <1230505007@ogr.klu.edu.tr>
2023-12-22 20:31:26 +0300

    Initial Commit

---
+++ 1230505007emirhan/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  1230505007emirhan
+//
+//  Created by Emirhan on 22.12.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
